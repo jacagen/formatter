b0b6a2f66395bdf0a82e3ecc38b2ae508a67e841
jcgentry <jcgentry@mac.com>
2021-12-29 16:31:04 -0500

    Improve formatting

--- phone-format-sam/format-phone-number.js
+++ phone-format-sam/format-phone-number.js
@@ -1,8 +1,17 @@
+/**
+ * Formats a ten-digit US phone number.
+ * 
+ * @param {string} input  the unformatted phone number
+ * @returns {string} the formatted number
+ * @throws {RangeError} if the input cannot be formatted 
+ */
 const formatPhoneNumber = (input) => {
-    console.log(`Received input ${input}`)
-    if (input.length != 10) throw new RangeError(`Bad format ${input}`)
-    else return input.slice(0, 3) + '-' + input.slice(3, 6) + '-' + input.slice(6)
-  }
-  
+  if (input.length != 10)
+    throw new RangeError(`Bad format ${input}`)
+  else
+    return input.slice(0, 3) + '-' +
+      input.slice(3, 6) + '-' +
+      input.slice(6)
+}
+
 module.exports = formatPhoneNumber
-  
